1489ce61bc8b0e9ad3d8fd9a74cabab9c2be7106
Isha1233 <ishac182003@gmail.com>
2025-12-10 00:30:58 +0530

    making deployment ready

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,11 +4,11 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import transactionRoutes from './routes/transactionRoutes.js'; // note the .js
-
+import path from "path";
 dotenv.config();
 
 const app = express();
-
+const _dirname=path.resolve();
 // Middleware
 app.use(cors({ origin: 'http://localhost:5173' }));
 app.use(express.json());
@@ -20,7 +20,10 @@ mongoose.connect(process.env.MONGO_URI)
 
 // Routes
 app.use('/api', transactionRoutes);
-
+app.use(express.static(path.join(_dirname,"/frontend/dist")))
+app.get('*',(_,res)=>{
+  res.sendFile(path.resolve(_dirname,"frontend","dist","index.html"));
+});
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
